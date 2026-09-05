eb2fe7ed5571cf02b6dfc7e7c21c1fe899da481a
Mally Dang <no-reply@mally.qzz.io>
2026-09-06 00:44:27 +0700

    feat: fillter the things

--- src-tauri/src/typescript/domModifier/index.ts
+++ src-tauri/src/typescript/domModifier/index.ts
@@ -1,52 +1,63 @@
 // modify the editor
 
 import { WebviewWindow } from "@tauri-apps/api/webviewWindow";
-import { waitForReduxStore } from "../helpers/getReactStore";
+import { getReduxStore, waitForReduxStore } from "../helpers/getReactStore";
 import { getDiscordRpc, setDiscordRpc } from "../storeManager";
 import { modifyCallbackPackageButton, modifyCallbackUploadButton, removeBackToHome, removeSeeProjectPage } from "./editor/menuBarModifier";
 import { alertOverwrite } from "./editor/overwriteMethods";
 import { addDesktopSettings } from "./editor/settings"
+import { emitTo } from "@tauri-apps/api/event";
 
 export async function modifyEditor() {
-    localStorage.setItem("penguin_discord_rpc", String(await getDiscordRpc()))
+    if (!window.location.href.toString().includes("packager")) {
+        localStorage.setItem("penguin_discord_rpc", String(await getDiscordRpc()))
 
-    modifyCallbackPackageButton(async () => {
-        const WINDOW_LABEL = 'packager-win';
+        modifyCallbackPackageButton(async () => {
+            const WINDOW_LABEL = 'packager-win';
+            const state = getReduxStore().getState()
 
-        WebviewWindow.getByLabel(WINDOW_LABEL)
-            .then(async (existingWindow) => {
-                if (existingWindow) {
-                    await existingWindow.unminimize();
-                    await existingWindow.setFocus();
-                } else {
-                    const packagerWindow = new WebviewWindow(WINDOW_LABEL, {
-                        url: 'packager/index.html',
-                        title: 'Packager',
-                        width: 800,
-                        height: 600,
-                    });
+            WebviewWindow.getByLabel(WINDOW_LABEL)
+                .then(async (existingWindow) => {
+                    if (existingWindow) {
+                        await existingWindow.unminimize();
+                        await existingWindow.setFocus();
+                    } else {
+                        const packagerWindow = new WebviewWindow(WINDOW_LABEL, {
+                            url: 'packager/index.html',
+                            title: 'Packager',
+                            width: 800,
+                            height: 600,
+                        });
 
-                    packagerWindow.once('tauri://error', (e) => {
-                        console.error('packager window failed to create', e);
-                    });
-                }
-            })
-            .catch(console.error);
-    });
+                        packagerWindow.once('tauri://error', (e) => {
+                            console.error('packager window failed to create', e);
+                        });
 
-    modifyCallbackUploadButton(async () => {
-        await alert("Unfortunately, we cannot auto upload to the penguinmod upload site. Therefore, please save your project and upload manually to the site.")
-        const store = await waitForReduxStore();
-        const state = store.getState();
+                        packagerWindow.once("tauri://webview-created", async () => {
+                            delay(500)
+                            emitTo("packager-win", "file://handoff", await state.vm.saveProjectSb3())
+                        })
+                    }
+                })
+                .catch(console.error);
+        });
 
-        window.open(`https://penguinmod.com/upload?name=${encodeURIComponent(state.scratchGui.projectTitle)}`);
-    });
-    addDesktopSettings(async (checkbox: boolean) => {
-        await setDiscordRpc(checkbox)
-    });
-    removeBackToHome();
-    removeSeeProjectPage();
-    alertOverwrite();
+        modifyCallbackUploadButton(async () => {
+            await alert("Unfortunately, we cannot auto upload to the penguinmod upload site. Therefore, please save your project and upload manually to the site.")
+            const store = await waitForReduxStore();
+            const state = store.getState();
+
+            window.open(`https://penguinmod.com/upload?name=${encodeURIComponent(state.scratchGui.projectTitle)}`);
+        });
+        addDesktopSettings(async (checkbox: boolean) => {
+            await setDiscordRpc(checkbox)
+        });
+        removeBackToHome();
+        removeSeeProjectPage();
+        alertOverwrite();
+    } else {
+        // TODO handle the json thing from vm
+    }
 }
 
 export { setupNativeClosePrompt } from "./editor/nativeClose";
